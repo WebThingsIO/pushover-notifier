695b01491c824ddfe5c06e681d74cd011c7f1647
Michael Stegeman <michael@stegeman.me>
2019-07-25 13:40:46 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,18 @@
+# pushover-notifier
+
+A simple WebThings Gateway notifier for sending push notifications via
+[Pushover](https://pushover.net).
+
+## Setup
+
+Log in to Pushover, add a new app, then copy the token.
+
+### Installation and Configuration
+
+On your gateway, navigate to _Settings -> Add-ons_. Next, click the plus (+)
+button and scroll down to install the Pushover Notifier.
+
+With the notifier installed, click "Configure", then input the token you just
+created and your user key.
+
+Now, you can go create some rules and use Pushover as a notification output.
---
+++ pushover-notifier.js
@@ -0,0 +1,80 @@
+const fetch = require('node-fetch');
+
+const {
+  Constants,
+  Notifier,
+  Outlet,
+} = require('gateway-addon');
+
+/**
+ * A Pushover outlet
+ */
+class PushoverOutlet extends Outlet {
+  /**
+   * @param {PushoverNotifier} notifier
+   * @param {string} id - A globally unique identifier
+   */
+  constructor(notifier, id, config) {
+    super(notifier, id);
+    this.name = 'Pushover';
+    this.config = config;
+  }
+
+  async notify(title, message, level) {
+    let priority = 0;
+
+    switch (level) {
+      case Constants.NotificationLevel.LOW:
+        priority = -1;
+        break;
+      case Constants.NotificationLevel.NORMAL:
+        priority = 0;
+        break;
+      case Constants.NotificationLevel.HIGH:
+        priority = 1;
+        break;
+    }
+
+    const body = {
+      token: this.config.token,
+      user: this.config.userKey,
+      title,
+      message,
+      priority,
+    };
+
+    // Let errors bubble up to gateway
+    await fetch('https://api.pushover.net/1/messages.json', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    }).then((res) => {
+      if (!res.ok) {
+        throw new Error(`Failed to post message: ${res.statusText}`);
+      }
+    });
+  }
+}
+
+/**
+ * Pushover Notifier
+ *
+ * Instantiates one Pushover outlet
+ */
+class PushoverNotifier extends Notifier {
+  constructor(addonManager, manifest) {
+    super(addonManager, 'pushover', manifest.name);
+
+    addonManager.addNotifier(this);
+
+    if (!this.outlets['pushover-0']) {
+      this.handleOutletAdded(
+        new PushoverOutlet(this, 'pushover-0', manifest.moziot.config)
+      );
+    }
+  }
+}
+
+module.exports = PushoverNotifier;
